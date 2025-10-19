165c0b7a79bea9d7f482843d78707a1e42f38ebe
avacinth <toribiohyacinth05@gmail.com>
2025-10-19 10:44:35 +0400

    Update strava_ml.py

--- strava_ml.py
+++ strava_ml.py
@@ -15,7 +15,7 @@ def pace_to_minutes(pace):
 
 df["avg_pace_min"] = df["avg_pace_per_km"].apply(pace_to_minutes)
 
-# Defining the activity type based on a criteria
+# Defining the activity type (gawagawa)
 def classify_activity(row):
     if row["distance_km"] < 0.5 and row["steps"] < 500:
         return "Sitting"
@@ -28,21 +28,20 @@ def classify_activity(row):
 
 df["activity_type"] = df.apply(classify_activity, axis=1)
 
-# Select relevant features
+# Printing the given data and the results (activity_type)
 features = ["distance_km", "avg_pace_min", "elapsed_time_min", "steps"]
 X = df[features]
 y = df["activity_type"]
 
-# Split the data
+# Splitting the data per column
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Train a model
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
 # Predict the activity type for all data
 df["predicted_activity"] = model.predict(X)
 
-# Show predictions per activity_id
+# Printing the predicted activity
 result = df[["activity_id", "distance_km", "avg_pace_min", "steps", "predicted_activity"]]
 print(result.to_string(index=False))
