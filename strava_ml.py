8f2c66bea0c7f831c4a09e476ef5628e1b418d0c
avacinth <toribiohyacinth05@gmail.com>
2025-11-08 13:32:52 +0400

    Update strava_ml.py

--- strava_ml.py
+++ strava_ml.py
@@ -3,15 +3,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
 # Load dataset
-df = pd.read_csv("strava_data_1.csv")
+df = pd.read_csv("public_dataset.csv")
 
-df["Activity ID"] = range(1, len(df) + 1)
+df["start_date_local"] = range(1, len(df) + 1)
 
-df["Total Steps"] = df["Total Steps"].replace({",": ""}, regex=True).astype(float)
+#df["Total Steps"] = df["Total Steps"].replace({",": ""}, regex=True).astype(float)
 
-features = ["Distance", "Average Speed", "Elapsed Time", "Elevation Gain", "Total Steps"] # Independent variables
+features = ["Distance", "Moving Time", "Elevation Gain", "Average Speed", "average_heartrate"] # Independent variables
 X = df[features]
-y = df["Activity Type"]  # Dependent variable
+y = df["type"]  # Dependent variable
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
@@ -23,5 +23,5 @@ model.fit(X_train, y_train)
 df["Predicted Activity Type"] = model.predict(X)
 
 # Print results
-result = df[["Activity ID", "Distance", "Average Speed", "Elapsed Time", "Elevation Gain", "Total Steps", "Predicted Activity Type"]]
+result = df[["Distance", "Moving Time", "Elevation Gain", "Average Speed", "average_heartrate", "Predicted Activity Type"]]
 print(result.to_string(index=False))
